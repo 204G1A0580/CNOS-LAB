d2092aadb00f249b48e57b87389d642a9934d7ed
204G1A0580 <112164777+204G1A0580@users.noreply.github.com>
2022-12-01 22:29:28 +0530

    Update priority.c

--- oslab/exp1/priority.c
+++ oslab/exp1/priority.c
@@ -1,42 +1,71 @@
 #include<stdio.h>
-main()
+ 
+int main()
 {
-int p[20],bt[20],pri[20], wt[20],tat[20],i, k, n, temp; float wtavg,
-tatavg;
-clrscr();
-printf("Enter the number of processes --- ");
-scanf("%d",&n);
-for(i=0;i<n;i++){
-p[i] = i;
-printf("Enter the Burst Time & Priority of Process %d --- ",i); scanf("%d
-%d",&bt[i], &pri[i]);
+    int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp,avg_wt,avg_tat;
+    printf("Enter Total Number of Process:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter Burst Time and Priority\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\nP[%d]\n",i+1);
+        printf("Burst Time:");
+        scanf("%d",&bt[i]);
+        printf("Priority:");
+        scanf("%d",&pr[i]);
+        p[i]=i+1;           //contains process number
+    }
+ 
+    //sorting burst time, priority and process number in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(pr[j]<pr[pos])
+                pos=j;
+        }
+ 
+        temp=pr[i];
+        pr[i]=pr[pos];
+        pr[pos]=temp;
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0; //waiting time for first process is zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
+    avg_wt=total/n;      //average waiting time
+    total=0;
+ 
+    printf("\nProcess\t    Burst Time    \tWaiting Time\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];     //calculate turnaround time
+        total+=tat[i];
+        printf("\nP[%d]\t\t  %d\t\t    %d\t\t\t%d",p[i],bt[i],wt[i],tat[i]);
+    }
+ 
+    avg_tat=total/n;     //average turnaround time
+    printf("\n\nAverage Waiting Time=%d",avg_wt);
+    printf("\nAverage Turnaround Time=%d\n",avg_tat);
+ 
+return 0;
 }
-for(i=0;i<n;i++)
-for(k=i+1;k<n;k++)
-if(pri[i] > pri[k]){
-temp=p[i];
-p[i]=p[k];
-p[k]=temp;
-temp=bt[i];
-bt[i]=bt[k];
-bt[k]=temp;
-temp=pri[i];
-pri[i]=pri[k];
-pri[k]=temp;
-}
-wtavg = wt[0] = 0;
-tatavg = tat[0] = bt[0];
-for(i=1;i<n;i++)
-{
-wt[i] = wt[i-1] + bt[i-1];
-tat[i] = tat[i-1] + bt[i];
-wtavg = wtavg + wt[i];
-tatavg = tatavg + tat[i];
-}
-printf("\nPROCESS\t\tPRIORITY\tBURST TIME\tWAITING TIME\tTURNAROUND
-TIME");
-for(i=0;i<n;i++)
-printf("\n%d \t\t %d \t\t %d \t\t %d \t\t %d ",p[i],pri[i],bt[i],wt[i],tat[i]);
-printf("\nAverage Waiting Time is --- %f",wtavg/n); printf("\nAverage
-Turnaround Time is --- %f",tatavg/n);
-getch();}
